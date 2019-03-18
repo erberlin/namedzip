6c7552e06b98e5d239788f9bc3f42967162a731f
Erik R Berlin <erberlin@users.noreply.github.com>
2019-03-17 20:04:16 -0700

    Add requirements for publishing to PyPI.

--- setup.py
+++ setup.py
@@ -1,3 +1,26 @@
+import pathlib
+
 from setuptools import setup
 
-setup(name="namedzip", version="0.3.0", py_modules=["namedzip"])
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README.rst").read_text()
+
+setup(
+    name="namedzip",
+    version="1.0.0",
+    description="Extends zip() and itertools.zip_longest() to generate named tuples.",
+    long_description=README,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/erberlin/namedzip",
+    author="Erik R Berlin",
+    author_email="erberlin.dev@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["namedzip"],
+    include_package_data=False,
+)
